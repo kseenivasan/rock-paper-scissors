3c0d84d7770d35456a89b73e2d5c8afcbb8fe035
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-16 19:39:32 +0530

    Remove test code after function verification

--- javascript.js
+++ javascript.js
@@ -38,7 +38,3 @@ function playRound(playerSelection, computerSelection) {
             }
     }
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
