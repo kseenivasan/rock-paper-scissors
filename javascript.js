94bbbde1d4cd5f44f28f1634b309b7627ccd3767
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-16 19:36:05 +0530

    Create playround function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,40 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random()*3);
     return choices[choice];
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            if (computerSelection === "rock") {
+                return "It's a tie!";
+            }
+            else if (computerSelection === "paper") {
+                return "You lose! Paper beats Rock";
+            }
+            else {
+                return "You win! Rock beats Scissors";
+            }
+        case "paper":
+            if (computerSelection === "rock") {
+                return "You win! Paper beats Rock";
+            }
+            else if (computerSelection === "paper") {
+                return "It's a tie!";
+            }
+            else {
+                return "You lose! Scissors beats Paper";
+            }
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You lose! Rock beats Scissors";
+            }
+            else if (computerSelection === "paper") {
+                return "You win! Scissors beats Paper";
+            }
+            else {
+                return "It's a tie!";
+            }
+    }
 }
