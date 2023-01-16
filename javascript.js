5ee9ae026ac54050ce2781c9e9ff6c08acf86e58
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-16 19:53:39 +0530

    Add game function and modify round function

--- javascript.js
+++ javascript.js
@@ -8,33 +8,49 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection.toLowerCase()) {
         case "rock":
             if (computerSelection === "rock") {
-                return "It's a tie!";
+                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "You lose! Paper beats Rock";
+                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "You win! Rock beats Scissors";
+                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
         case "paper":
             if (computerSelection === "rock") {
-                return "You win! Paper beats Rock";
+                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "It's a tie!";
+                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "You lose! Scissors beats Paper";
+                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
         case "scissors":
             if (computerSelection === "rock") {
-                return "You lose! Rock beats Scissors";
+                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "You win! Scissors beats Paper";
+                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "It's a tie!";
+                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
     }
+}
+function game() {
+    let choice;
+    let score;
+    for (let i = 0; i < 5; i++) {
+        choice = prompt("Playing round " + (i + 1) +", please enter your choice (rock, paper, scissors)");
+        let result = playRound(choice,getComputerChoice());
+        console.log(result);
+        if (result.charAt(0) === "Y") {
+            if (result.charAt(4) === "w") {
+                score++;
+            }
+        }
+    }
+
+    console.log("Thank you for playing! You played 5 rounds of Rock-Paper-Scissors, and won" + score + "times");
 }
