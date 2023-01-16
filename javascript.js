e884c81e10867a3fa652981d3416bfcdb3f74eca
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-16 19:57:22 +0530

    Fix error with score tracking

--- javascript.js
+++ javascript.js
@@ -8,39 +8,39 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection.toLowerCase()) {
         case "rock":
             if (computerSelection === "rock") {
-                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "It's a tie! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You lose! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You win! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
         case "paper":
             if (computerSelection === "rock") {
-                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You win! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "It's a tie! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You lose! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
         case "scissors":
             if (computerSelection === "rock") {
-                return "You lose! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You lose! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else if (computerSelection === "paper") {
-                return "You win! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "You win! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
             else {
-                return "It's a tie! Your choice:" + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
+                return "It's a tie! Your choice: " + playerSelection.toLowerCase() + " Computer choice: " + computerSelection;
             }
     }
 }
 function game() {
     let choice;
-    let score;
+    let score = 0;
     for (let i = 0; i < 5; i++) {
         choice = prompt("Playing round " + (i + 1) +", please enter your choice (rock, paper, scissors)");
         let result = playRound(choice,getComputerChoice());
@@ -52,5 +52,7 @@ function game() {
         }
     }
 
-    console.log("Thank you for playing! You played 5 rounds of Rock-Paper-Scissors, and won" + score + "times");
+    console.log("Thank you for playing! You played 5 rounds of Rock-Paper-Scissors, and won " + score + " times");
 }
+
+game();
